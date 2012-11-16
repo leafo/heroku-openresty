04dd59b0ef9e141248d7969b0b2bd91588a4562d
leaf corcoran <leafot@gmail.com>
2012-11-15 21:07:42 -0800

    add support for interpolating postgres connection information

--- bin/compile_nginx_config.lua
+++ bin/compile_nginx_config.lua
@@ -1,8 +1,27 @@
 #!/usr/bin/env lua
 
+local filters = {
+  pg = function (url)
+    local user, password, host, db = url:match "^postgres://(.*):(.*)@(.*)/(.*)$"
+    return ("%s dbname=%s user=%s password=%s"):format(host, db, user, password)
+  end
+}
+
 function fill_env(s)
   return (s:gsub('(${%b{}})', function(w)
-    return os.getenv(w:sub(4, -3)) or w
+    local name = w:sub(4, -3)
+
+    local filter, env = name:match("^(%S*)%s*(.*)$")
+    if filter and filters[filter] then
+      env = os.getenv(env)
+      if not env then
+        return w
+      end
+
+      return filters[filter](env)
+    else
+      return os.getenv(name) or w
+    end
   end))
 end
 
--- test/start-test.sh
+++ test/start-test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PORT=8080 PATH="$ROOT/../bin:/usr/local/openresty/nginx/sbin:$PATH" start_nginx.sh "$ROOT/nginx.conf" "$ROOT"
+PORT=8080 PATH="$ROOT/../bin:/usr/local/openresty/nginx/sbin:$PATH" POSTGRES_URL="postgres://theuser:thepassword@127.0.0.1/cats" start_nginx.sh "$ROOT/nginx.conf" "$ROOT"
