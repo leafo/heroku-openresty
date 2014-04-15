2f7d508238499e198a169ed15e34f1a40e6dec6e
leaf corcoran <leafot@gmail.com>
2014-04-14 23:22:02 -0700

    update build script

--- support/build-openresty.sh
+++ support/build-openresty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 base_dir=$(pwd)
-openresty_tar="ngx_openresty-1.2.8.6.tar.gz"
+openresty_tar="ngx_openresty-1.5.11.1.tar.gz"
 pcre_tar="pcre-8.33.tar.gz"
 
 echo "$(tput setaf 2)Downloading openresty...$(tput sgr0)"
@@ -20,7 +20,7 @@ pcre_dir="$base_dir/$(ls -d pcre*/ | head -n 1)"
 echo "$(tput setaf 2)Building openresty...$(tput sgr0)"
 (
 	cd $(ls -d ngx_openresty*/ | head -n 1)
-	./configure --with-pcre=$pcre_dir --with-luajit --with-http_postgres_module
+	./configure --with-pcre=$pcre_dir --with-http_postgres_module
 	make
 	mkdir /tmp/openresty
 	make DESTDIR=/tmp/openresty install
